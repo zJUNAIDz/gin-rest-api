4b6caef3010c37d03259673e3080133d1ed088a9
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-01-13 12:51:18 +0530

    add db.go

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+var err error
+func InitDB() {
+	DB, err = sql.Open("sqlite3", "api.db")
+	if err != nil {
+		panic("failed to Initialize DB")
+	}
+	DB.SetMaxOpenConns(5)
+	DB.SetConnMaxIdleTime(1)
+	CreateTable()
+}
+
+func CreateTable() {
+	createTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		userID INTEGER
+	)
+	`
+	_, err := DB.Exec(createTable)
+	if err != nil {
+		panic("failed to create table"+ err.Error())
+	}
+}
