fefbe95d94d8cc69ca314f6380e618911412a409
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-01-14 13:17:15 +0530

    group routes to use auth middleware for protected routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zjunaidz/gin-rest-api/middlewares"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	// Register all routes here
+	//*protected routes group
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authentication)
+	authenticated.PUT("/events/update/:id", updateEventById)
+	authenticated.POST("/events/create", createNewEvent)
+	authenticated.DELETE("/events/delete/:id", deleteEventById)
+
 	server.GET("/events", getAllEvents)
 	server.GET("/events/:id", getEventById)
-	server.PUT("/events/update/:id", updateEventById)
-	server.POST("/events/create", createNewEvent)
-	server.DELETE("/events/delete/:id", deleteEventById)
 	server.GET("/users", getAllUsers)
 	server.POST("/signup", signup)
 	server.POST("/login", login)
